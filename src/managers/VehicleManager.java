dc5c4ab3fbaee66278faccb9f452d92f06e7dd93
nikol <nikol@192.168.1.228>
2026-05-11 17:31:07 +0300

    ψομμιτ

--- src/managers/VehicleManager.java
+++ src/managers/VehicleManager.java
@@ -1,5 +1,34 @@
 package managers;
 
-public class VehicleManager {
+import entities.vehicles.Vehicle;
+import storage.StorableList;
 
+public class VehicleManager {
+    private StorableList<Vehicle> fleet;
+
+    public VehicleManager(StorableList<Vehicle> loadedFleet) {
+        this.fleet = loadedFleet;
+    }
+
+    // Προσθήκη νέου οχήματος 
+    public void addVehicle(Vehicle vehicle) {
+        fleet.add(vehicle);
+    }
+
+    // Έλεγχος διαθεσιμότητας και επιστροφή κατάλληλου οχήματος 
+    // (Στην πράξη θα πρέπει να ελέγχει και τα ενεργά συμβόλαια για να δει αν λείπει)
+    public Vehicle findAvailableVehicle(String category, String type) {
+        for (int i = 0; i < fleet.size(); i++) {
+            Vehicle v = fleet.get(i);
+            if (v.getCategory().equals(category)) {
+                // Εδώ θα έμπαινε η λογική: "Είναι ενοικιασμένο αυτή τη στιγμή;"
+                return v;
+            }
+        }
+        return null; // Δεν βρέθηκε διαθέσιμο
+    }
+
+    public StorableList<Vehicle> getFleet() {
+        return fleet;
+    }
 }
