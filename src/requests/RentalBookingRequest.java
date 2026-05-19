eecde17610027c23ba1613b41cbf0d628ee831d6
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:34:57 +0300

    Update RentalBookingRequest.java

--- src/requests/RentalBookingRequest.java
+++ src/requests/RentalBookingRequest.java
@@ -1,5 +1,21 @@
 package requests;
 
-public class RentalBookingRequest {
+import java.time.LocalDateTime;
 
+public class RentalBookingRequest extends Request {
+	private String vehicleLicensePlate;
+	private LocalDateTime rentalStartDate;
+	private LocalDateTime rentalEndDate;
+	
+	public RentalBookingRequest(String requestId, String customerVat, String vehicleLicensePlate, LocalDateTime start, LocalDateTime end) {
+		super(requestId, vehicleLicensePlate, customerVat);
+		this.vehicleLicensePlate = vehicleLicensePlate;
+		this.rentalStartDate = start;
+		this.rentalEndDate = end;
+	}
+	
+	public String getVehicleLicensePlate() { return vehicleLicensePlate; }
+	public LocalDateTime getRentalStartDate() { return rentalStartDate; } 
+	public LocalDateTime getRentalEndDate() { return rentalEndDate; } 
+	
 }
