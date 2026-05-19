cc1a78995ed893bce213508e1eac844ac4a1ec85
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:31:46 +0300

    Update Credit.java

--- src/finance/Credit.java
+++ src/finance/Credit.java
@@ -1,5 +1,14 @@
 package finance;
 
-public class Credit {
-
+public class extends Transaction  {
+	public enum CreditType { CUSTOMER_PAYMENT, CONTRACT_REFUND }
+	private CreditType type;
+	
+	public Credit(String id, String refId, double amount, String desc, CreditType type) {
+		super(id, refId, amount, desc);
+		this.type = type;
+	}
+	
+	public CreditType getType() { return type; }
+	
 }
