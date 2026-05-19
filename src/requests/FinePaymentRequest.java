1d0c15b6122148be1828303e1cef945f1754932a
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:34:00 +0300

    Update FinePaymentRequest.java

--- src/requests/FinePaymentRequest.java
+++ src/requests/FinePaymentRequest.java
@@ -1,5 +1,18 @@
 package requests;
 
-public class FinePaymentRequest {
+import java.time.LocalDateTime;
 
+public class FinePaymentRequest extends Request {
+	private String vehicleLicensePlate;
+	private LocalDateTime noticeDate;
+	
+	public FinePaymentRequest(String requestId, String vehicleLicensePlate, LocalDateTime noticeDate) {
+		super(requestId, vehicleLicensePlate, null);
+		this.vehicleLicensePlate = vehicleLicensePlate;
+		this.noticeDate = noticeDate;
+	}
+	
+	public String getVehicleLicensePlate() { return vehicleLicensePlate; }
+	public LocalDateTime getNoticeeDate() { return noticeDate; }
+	
 }
