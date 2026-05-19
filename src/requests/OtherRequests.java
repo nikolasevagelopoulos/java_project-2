d31ccd8654e2ea4753bb7ce0e030e95b4a19cdd4
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:34:23 +0300

    Update OtherRequests.java

--- src/requests/OtherRequests.java
+++ src/requests/OtherRequests.java
@@ -1,5 +1,33 @@
 package requests;
 
 public class OtherRequests {
-
+ 
+	   public static class RentalCancelationRequest extends Request {
+		   private String contractId;
+		   
+		   public RentalCancelationRequest(String requestId, String customerVat, String contractId) {
+			   super(requestId, contractId, customerVat);
+			   this.contractId = contractId;
+		   }
+		   
+		   public String getContractId() { return contractId; }
+		   
+	   }
+	   
+	   public static class RentalReturnRequest extends Request {
+		   private String contractId;
+		   
+		   public RentalReturnRequest(String requestId, String customerVat, String contractId) {
+			   super(requestId, contractId, customerVat);
+			   this.contractId = contractId;
+		   }
+		   
+		   public String getContractId() { return contractId; }
+	   }
+	   
+	   public static class CustomerPaymentRequest extends Request {
+		    public CustomerPaymentRequest(String requestId, String customerVat) {
+		    	super(requestId, null, customerVat);
+		    }
+	   }
 }
