578403914a580da979841971caec71d6a0ad7af2
nikolasevagelopoulos <nikolasevagelopoulos@gmail.com>
2026-05-19 16:47:25 +0300

    Update Individual.java

--- src/entities/user/Individual.java
+++ src/entities/user/Individual.java
@@ -1,5 +1,25 @@
 package entities.user;
 
-public class Individual {
+public class Individual extends Customer {
+    public Individual(String username, String password, String vat) {
+        super(username, password, "INDIVIDUAL", vat);
+    }
 
+    public Individual() {
+        super();
+    }
+
+    @Override
+    public String toCSV() {
+        return "INDIVIDUAL," + username + "," + password + "," + vat;
+    }
+
+    @Override
+    public void fromCSV(String csvLine) {
+        String[] parts = csvLine.split(",");
+        this.role = parts[0];
+        this.username = parts[1];
+        this.password = parts[2];
+        this.vat = parts[3];
+    }
 }
