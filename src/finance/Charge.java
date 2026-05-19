7323317dbd14d13063e1c5c47dcc8c1b4840ccd6
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:31:14 +0300

    Update Charge.java

--- src/finance/Charge.java
+++ src/finance/Charge.java
@@ -1,5 +1,14 @@
 package finance;
 
-public class Charge {
-
+public class Charge extends Transaction {
+	public enum ChargeType { RENTAL, OVERDUE, FINE }
+	private ChargeType type;
+	
+	public Charge(String id, String refId, double amount, String desc, ChargeType type) {
+		super(id, refId, amount, desc);
+		this.type = type;
+	}
+	
+	public ChargeType getType() { return type; }
+	
 }
