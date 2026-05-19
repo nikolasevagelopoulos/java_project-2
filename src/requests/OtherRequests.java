7da02951fceb69c70ffe9e941b885af54acd3161
nikolasevagelopoulos <nikolasevagelopoulos@gmail.com>
2026-05-19 16:50:31 +0300

    Refactor OtherRequests to include request types

--- src/requests/OtherRequests.java
+++ src/requests/OtherRequests.java
@@ -2,32 +2,30 @@ package requests;
 
 public class OtherRequests {
  
-	   public static class RentalCancelationRequest extends Request {
-		   private String contractId;
-		   
-		   public RentalCancelationRequest(String requestId, String customerVat, String contractId) {
-			   super(requestId, contractId, customerVat);
-			   this.contractId = contractId;
-		   }
-		   
-		   public String getContractId() { return contractId; }
-		   
-	   }
-	   
-	   public static class RentalReturnRequest extends Request {
-		   private String contractId;
-		   
-		   public RentalReturnRequest(String requestId, String customerVat, String contractId) {
-			   super(requestId, contractId, customerVat);
-			   this.contractId = contractId;
-		   }
-		   
-		   public String getContractId() { return contractId; }
-	   }
-	   
-	   public static class CustomerPaymentRequest extends Request {
-		    public CustomerPaymentRequest(String requestId, String customerVat) {
-		    	super(requestId, null, customerVat);
-		    }
-	   }
+    public static class RentalCancelationRequest extends Request {
+        public RentalCancelationRequest(String requestId, String customerVat, String contractId) {
+            super(requestId, contractId, customerVat);
+        }
+        
+        @Override
+        public String getType() { return "CANCEL"; }
+    }
+    
+    public static class RentalReturnRequest extends Request {
+        public RentalReturnRequest(String requestId, String customerVat, String contractId) {
+            super(requestId, contractId, customerVat);
+        }
+        
+        @Override
+        public String getType() { return "RETURN"; }
+    }
+    
+    public static class CustomerPaymentRequest extends Request {
+        public CustomerPaymentRequest(String requestId, String customerVat) {
+            super(requestId, null, customerVat);
+        }
+
+        @Override
+        public String getType() { return "PAYMENT"; }
+    }
 }
