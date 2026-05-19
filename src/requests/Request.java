b427a2c2758d85b0eeb9e4393f80e92dcb162a2a
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:35:34 +0300

    Update Request.java

--- src/requests/Request.java
+++ src/requests/Request.java
@@ -1,5 +1,23 @@
 package requests;
 
-public class Request {
+import java.time.LocalDateTime;
+
+public abstract class Request {
+	private String requestId;
+	private String referenceId;
+	private LocalDateTime timestamp;
+	private String customerVat;
+	
+	public Request(String requestId, String referenceId, String customerVat) {
+		this.requestId = requestId;
+		this.referenceId = referenceId;
+		this.customerVat = customerVat;
+		this.timestamp = LocalDateTime.now();
+	}
+	
+	public String getRequestId() { return requestId; }
+	public String getReferenceId() { return referenceId; }
+	public LocalDateTime getTimestamp() { return timestamp; }
+	public String getCustomerVat() { return customerVat; }
 
 }
