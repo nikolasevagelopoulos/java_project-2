677c02c79c09b1cfc0565275b68a48806eaf5fe2
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:32:55 +0300

    Update Transaction.java

--- src/finance/Transaction.java
+++ src/finance/Transaction.java
@@ -1,5 +1,27 @@
 package finance;
 
-public class Transaction {
+import java.time.LocalDateTime;
 
-}
+public abstract class Transaction {
+	private String transactionId;
+	private String referenceId;
+	private LocalDateTime timestamp;
+	private double amount;
+	private String description;
+	
+	
+	public Transaction(String transactionId, String refernceId, double mount, String description) {
+		this.transactionId = transactionId;
+		this.referenceId = referenceId;
+		this.amount = amount;
+		this.description = description;
+		this.timestamp = LocalDateTime.now();
+	}
+	
+	public double getAmount() { return amount; }
+	public String getTransactionId() { return transactionId; }
+	public String getReferenceId() { return referenceId; } 
+	public String getDescription() { return description; }
+	public LocalDateTime getTimestamp() { return timestamp; }
+	}
+	
