9f1c6cdac4fb4382dfbca67672bcd36580a75e1a
nikolasevagelopoulos <nikolasevagelopoulos@gmail.com>
2026-05-19 16:47:06 +0300

    Implement Admin class extending User

--- src/entities/user/Admin.java
+++ src/entities/user/Admin.java
@@ -1,5 +1,24 @@
 package entities.user;
 
-public class Admin {
+public class Admin extends User {
+    public Admin(String username, String password) {
+        super(username, password, "ADMIN");
+    }
+    
+    public Admin() {
+        super();
+    }
 
+    @Override
+    public String toCSV() {
+        return "ADMIN," + username + "," + password;
+    }
+
+    @Override
+    public void fromCSV(String csvLine) {
+        String[] parts = csvLine.split(",");
+        this.role = parts[0];
+        this.username = parts[1];
+        this.password = parts[2];
+    }
 }
