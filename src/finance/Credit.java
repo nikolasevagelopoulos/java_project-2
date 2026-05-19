66d5554e82d8c8119845cb3bdd2a30e45884d7d0
nikolasevagelopoulos <nikolasevagelopoulos@gmail.com>
2026-05-19 16:48:27 +0300

    Fix class declaration for Credit

--- src/finance/Credit.java
+++ src/finance/Credit.java
@@ -1,14 +1,13 @@
 package finance;
 
-public class extends Transaction  {
-	public enum CreditType { CUSTOMER_PAYMENT, CONTRACT_REFUND }
-	private CreditType type;
-	
-	public Credit(String id, String refId, double amount, String desc, CreditType type) {
-		super(id, refId, amount, desc);
-		this.type = type;
-	}
-	
-	public CreditType getType() { return type; }
-	
+public class Credit extends Transaction {
+    public enum CreditType { CUSTOMER_PAYMENT, CONTRACT_REFUND }
+    private CreditType type;
+    
+    public Credit(String id, String refId, double amount, String desc, CreditType type) {
+        super(id, refId, amount, desc);
+        this.type = type;
+    }
+    
+    public CreditType getType() { return type; }
 }
