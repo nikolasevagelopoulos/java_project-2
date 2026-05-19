caf9e9686c29cb338ab1d2a264863b84f1089c98
nikolasevagelopoulos <nikolasevagelopoulos@gmail.com>
2026-05-19 16:50:12 +0300

    Add amount field and update constructor in FinePaymentRequest

--- src/requests/FinePaymentRequest.java
+++ src/requests/FinePaymentRequest.java
@@ -3,16 +3,23 @@ package requests;
 import java.time.LocalDateTime;
 
 public class FinePaymentRequest extends Request {
-	private String vehicleLicensePlate;
-	private LocalDateTime noticeDate;
-	
-	public FinePaymentRequest(String requestId, String vehicleLicensePlate, LocalDateTime noticeDate) {
-		super(requestId, vehicleLicensePlate, null);
-		this.vehicleLicensePlate = vehicleLicensePlate;
-		this.noticeDate = noticeDate;
-	}
-	
-	public String getVehicleLicensePlate() { return vehicleLicensePlate; }
-	public LocalDateTime getNoticeeDate() { return noticeDate; }
-	
+    private String vehicleLicensePlate;
+    private LocalDateTime noticeDate;
+    private double amount;
+    
+    public FinePaymentRequest(String requestId, String referenceId, String vehicleLicensePlate, LocalDateTime noticeDate, double amount) {
+        super(requestId, referenceId, null); // VAT is tracked indirectly through licensePlate and date [cite: 65]
+        this.vehicleLicensePlate = vehicleLicensePlate;
+        this.noticeDate = noticeDate;
+        this.amount = amount;
+    }
+    
+    public String getVehicleLicensePlate() { return vehicleLicensePlate; }
+    public LocalDateTime getNoticeDate() { return noticeDate; }
+    public double getAmount() { return amount; }
+
+    @Override
+    public String getType() {
+        return "FINE";
+    }
 }
