90dd281dd4ad2c9a4fae9cad6caf90494425a930
lelekenamaria <lelekenamaria@yahoo.com>
2026-05-19 16:33:24 +0300

    Update Wallet.java

--- src/finance/Wallet.java
+++ src/finance/Wallet.java
@@ -1,5 +1,35 @@
 package finance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet {
+	private double balance;
+	private List<Statement> history;
+	
+	public Wallet() {
+		this.balance = 0.0;
+		this.history = new ArrayList<>();
+		
+	}
+	
+	public void addTransaction(Transaction tx, String creatorName) {
+		if (tx instanceof Charge) {
+			balance += tx.getAmount();
+		} else if (tx instanceof Credit) {
+			balance -= tx.getAmount();
+		}
+		
+		history.add(new Statement(tx, creatorName));
+		
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public List<Statement> getHistory() {
+		return new ArrayList<>(history);
+	}
 
 }
